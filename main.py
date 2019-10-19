833403c3df5fc6c7cb2e6b7680b52617b943d1bb
George <georgesut@yahoo.com>
2019-10-19 17:31:45 +0300

    Fix the accessing of the transaction list by mentioning the date index; Test passed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import date
+import datetime
 
 """
     TODO: Change the transaction_list specification in the <ui_add>
@@ -25,10 +25,29 @@ def ui_add(transaction_list, value, type, description):
         > description - A description of the transaction:
                         E.g: "add 100 out PIZZA"
     """
-    current_date = date.today() ### The current date when the transaction
-                                ### is made.
+    current_date = datetime.datetime.today().day ### The current date when the
+                                                 ### transaction is made.
     new_transaction = (current_date, value, type, description)
-    transaction_list.append(new_transaction)
+    transaction_list[current_date - 1].append(new_transaction)
+
+def test_add():
+    account_transactions = [[] for i in range(31)]
+    correct_result = [[] for i in range(31)]
+
+    today = datetime.datetime.today().day
+    test_value = 100
+    test_type = "out"
+    test_description = "pizza"
+
+    ui_add(account_transactions, test_value, test_type, test_description)
+    correct_result[today - 1].append((today, test_value, test_type, test_description))
+
+    assert(account_transactions == correct_result)
+
+def run_all_tests():
+    test_add()
 
 def main():
     pass
+
+run_all_tests()
