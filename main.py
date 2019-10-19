ddb03fadb4e69df33d951f4fdba12b6b283d2d86
George <georgesut@yahoo.com>
2019-10-19 16:59:55 +0300

    Add ui_add function and its specification

---
+++ main.py
@@ -0,0 +1,25 @@
+
+def ui_add(transactions, value, type, description):
+    """
+    Adds to a transaction list a new transaction on the current day.
+    Input:
+        > transactions - The list of transactions
+
+        > value - A positive integer which represents the amount of
+                  money that was transferred
+
+        > type - A string which represents the type of the transaction.
+                 It can be one of the following:
+                        * in - a sum of money was transferred into the
+                               account
+                        * out - a sum of money was transferred from the
+                               account to somewhere else
+
+        > description - A description of the transaction:
+                        E.g: "add 100 out PIZZA"
+    """
+
+    pass
+
+def main():
+    pass
