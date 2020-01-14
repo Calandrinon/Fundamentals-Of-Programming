c005630ec61f89f5b718972baf32509839344806
George <georgesut@yahoo.com>
2020-01-14 21:39:48 +0200

    Add iterative version

--- main.py
+++ main.py
@@ -20,4 +20,36 @@ def generate_option_alternatives_recursively(solution_length, index=0, number_of
             generate_option_alternatives_recursively(solution_length, index+1, number_of_1_options, new_result)
 
 
+def generate_option_alternatives_iteratively(solution_length, index=0, number_of_1_options=0, result=""):
+    stack = []
+    stack.append((solution_length, index, number_of_1_options, result))
+
+    while len(stack) > 0:
+        parameter_solution_length, parameter_index, parameter_number_of_1_options, parameter_result = stack[-1]
+        del stack[-1]
+
+        options = ['1', 'X', '2']
+
+        if parameter_index == parameter_solution_length:
+            print(parameter_result)
+            continue
+
+        if parameter_index == parameter_solution_length - 1:
+            del options[1]
+
+
+        for option in options:
+            new_result = parameter_result
+
+            if option == '1':
+                if parameter_number_of_1_options < 2:
+                    new_result += option
+                    stack.append((parameter_solution_length, parameter_index+1, parameter_number_of_1_options+1, new_result))
+            else:
+                new_result += option
+                stack.append((parameter_solution_length, parameter_index+1, parameter_number_of_1_options, new_result))
+
+
+
 generate_option_alternatives_recursively(4)
+#generate_option_alternatives_iteratively(4)
